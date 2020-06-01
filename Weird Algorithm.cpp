ac1020bf5b8046c1d757f3c48e2f9c9fb2df6268
Prishita Kadam <55227184+prishitakadam@users.noreply.github.com>
2020-06-01 21:56:38 +0530

    Update Weird Algorithm.cpp

--- Weird Algorithm.cpp
+++ Weird Algorithm.cpp
@@ -8,22 +8,16 @@ int main()
     long int n;
     
     cin >> n;
-    
     cout << n << " ";
     
-    
     while(n!= 1){
-        
         if(n%2 == 0){
-            
             n = n/2;
         }
         else{
-            
             n = n * 3 + 1;
         }
         cout << n << " ";
     }
-
     return 0;
 }
